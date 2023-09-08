6680f47e545e9a6f190e69a99f859b59c1079419
Shahabr99 <sean.rohani9@gmail.com>
2023-09-08 15:46:37 -0700

    part2 started

---
+++ src/TodoList.js
@@ -0,0 +1,20 @@
+import React, {useState} from 'react';
+import NewTodoForm from 'react';
+
+
+const TodoList = () => {
+
+
+  return (
+    <>
+      <div>
+        <NewTodoForm />
+      </div>
+      <div>
+
+      </div>
+    </>
+  )
+}
+
+export default TodoList;
