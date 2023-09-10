3107d8caf43391392d688d50134e8574c7ec632f
Shahabr99 <sean.rohani9@gmail.com>
2023-09-09 17:51:34 -0700

    close btn added

--- src/Todo.js
+++ src/Todo.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Todo.css';
 
+
 const Todo = ({ topic, task }) => {
+
+  const [isClosed, setIsClosed] = useState(true);
+
+  const handleClick = () => {
+    setIsClosed(false)
+  }
+
   return (
-    <div className="task">
+    <>
+      {isClosed && <div className="task">
+        <span className= "close-btn" onClick={handleClick}>X</span>
         <h4>{topic}</h4>
         <p>{task}</p>
-    </div>
+      </div>}
+    </>
+    
   )
 }
 
